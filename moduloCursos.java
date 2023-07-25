25612e50255244d58a7ddd8f977a1eceb051c20f
saraycubillos <62775794+saraycubillos@users.noreply.github.com>
2023-07-25 13:26:24 -0500

    Open/Closed

---
+++ moduloCursos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package openClosed;
+
+/* Implementación del módulo de cursos
+ */
+public class moduloCursos implements PortalModule {
+    public void display() {
+        System.out.println("Mostrando los cursos disponibles");
+    }
+}
