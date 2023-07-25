91d509b344f23d3661655c20021b165a0cf9354e
saraycubillos <62775794+saraycubillos@users.noreply.github.com>
2023-07-25 13:34:03 -0500

    Add files via upload

---
+++ DependencyInversion/badImplementation/DatabaseService.java
@@ -0,0 +1,11 @@
+
+package dependencyInversion.badImplementation;
+
+/* Mala implementación de la inversión de dependencias
+
+ */
+public class DatabaseService {
+    //...
+    void getDatos(){ //... 
+    }
+}
